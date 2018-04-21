ffb7b45e91515e7f4824ac33c4284eac7678b7a7
Peter Arthur <peterdarthur@gmail.com>
2018-04-20 19:11:26 -0700

    add Google Analytics, set <head> as php include

--- wp-content/themes/startbootstrap-freelancer-gh-pages/index.php
+++ wp-content/themes/startbootstrap-freelancer-gh-pages/index.php
@@ -3,30 +3,12 @@
 
   <head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <php include_once "includes/head_contents.php"; ?>
 
-    <title>Freelancer - Start Bootstrap Theme</title>
+    <meta name="description" content="Peter Arthur is a web developer with extensive design and writing experience.">
+    <meta name="author" content="Peter Arthur">
 
-    <!-- Bootstrap core CSS -->
-    <link href="/wp-content/themes/startbootstrap-freelancer-gh-pages/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="/wp-content/themes/startbootstrap-freelancer-gh-pages/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- Plugin CSS -->
-    <link href="/wp-content/themes/startbootstrap-freelancer-gh-pages/vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
-
-    <!-- Custom styles for this template -->
-    <link href="/wp-content/themes/startbootstrap-freelancer-gh-pages/css/freelancer.min.css" rel="stylesheet">
-
-    <!-- Custom styles -->
-    <link href="/wp-content/themes/startbootstrap-freelancer-gh-pages/style.css" rel="stylesheet">
-
+    <title>Home • Peter Arthur</title>
 
   </head>
 
